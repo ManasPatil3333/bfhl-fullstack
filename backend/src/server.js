7ddea6d2077586f12f2fbca904261ced7cc5e45e
PatilManas3333 <patilmanas3333@gmail.com>
2026-04-24 18:54:34 +0530

    fix: add dynamic port for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const app = require('./app');
 
+// 🔥 IMPORTANT CHANGE
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
